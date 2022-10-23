9237333984acf4b5cb8729bc51fcbdda0a3b062d
Parminder Singh <parminder94171@gmail.com>
2022-10-23 12:04:05 +0530

    FEAT : Used Ref instead of state

--- src/components/NewUser/NewUser.js
+++ src/components/NewUser/NewUser.js
@@ -2,78 +2,74 @@ import Card from "../UI/Card/Card";
 import Button from "../UI/Button/Button";
 import styles from "./NewUser.module.css";
 import Modal from "../UI/Modal/Modal";
-import { useState } from "react";
+import { useRef, useState } from "react";
 
-const NewUser= (props)=> {
-    const defaultUserInput = {
-        username: "", 
-        age: ""
-    }
+const NewUser = (props) => {
+    const usernameRef = useRef();
+    const ageRef = useRef();
 
     const defaultError = {
-        title: "", 
+        title: "",
         message: "",
-        isVisible: 0
-    }
-    
+        isVisible: 0,
+    };
+
     const [error, setError] = useState(defaultError);
 
-    const [userInput, setUserInput] = useState(defaultUserInput);
-    const submitFormHandler = (event)=> {
+    const submitFormHandler = (event) => {
         event.preventDefault();
-        userInput.age = +userInput.age;
-        if (userInput.age < 0) {
+        if (+ageRef.current.value < 0) {
             setError({
-                title : "Invalid Age",
+                title: "Invalid Age",
                 message: "Age Can not be negative",
-                isVisible: 1
+                isVisible: 1,
             });
             return;
         }
         props.onAddNewUser({
             id: new Date().getTime().toString(),
-            ...userInput,
+            username: usernameRef.current.value,
+            age: +ageRef.current.value
         });
-        setUserInput(defaultUserInput);
-    }
-    
-    const usernameChangeHandler = (event)=> {
-        setUserInput(prevState=>{
-            return {
-                ...prevState,
-                username: event.target.value
-            }
-        })
-    }
-    
-    const ageChangeHandler = (event)=> {
-        setUserInput(prevState=>{
-            return {
-                ...prevState,
-                age: event.target.value
-            }
-        })
-    }
+        usernameRef.current.value = "";
+        ageRef.current.value = "";
+    };
 
-    const closeModalHandler = () =>{
+    const closeModalHandler = () => {
         setError(defaultError);
-    }
+    };
 
     return (
         <Card>
             <form onSubmit={submitFormHandler}>
                 <div className={styles.input}>
                     <label>Username</label>
-                    <input type="text" value={userInput.username} onChange={usernameChangeHandler} required />
+                    <input
+                        type="text"
+                        ref={usernameRef}
+                        required
+                    />
                 </div>
                 <div className={styles.input}>
                     <label>Age (Years)</label>
-                    <input type="number" value={userInput.age} onChange={ageChangeHandler} required/>
+                    <input
+                        type="number"
+                        ref={ageRef}
+                        required
+                    />
                 </div>
-                <Button type="submit" >Add User</Button>
+                <Button type="submit">Add User</Button>
             </form>
-            {error.isVisible ? <Modal title={error.title} message={error.message} onClose={closeModalHandler}/>  : ""}
+            {error.isVisible ? (
+                <Modal
+                    title={error.title}
+                    message={error.message}
+                    onClose={closeModalHandler}
+                />
+            ) : (
+                ""
+            )}
         </Card>
     );
-}
+};
 export default NewUser;
